3eedfa196cd861ac33cb958f15be23c22a1cad40
Jeevan Kishore <118504739+Jeevan177@users.noreply.github.com>
2025-03-20 08:14:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -54,8 +54,7 @@ def get_conversational_chain():
 
 
 def user_input(user_question, vector_store):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
+  
     # new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=False)
     docs = vector_store.similarity_search(user_question)
 
